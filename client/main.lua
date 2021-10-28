5f113a2ec464e7bd33851624fbe320d988424d39
Nevermind <58900773+RalivTV@users.noreply.github.com>
2021-10-28 11:52:09 +0100

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,15 +1,3 @@
-Keys = {
-	["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
-	["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
-	["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
-	["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
-	["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
-	["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
-	["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
-	["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
-	["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
-}
-
 local FirstSpawn, IsBusy = true, false
 
 IsDead = false
@@ -33,6 +21,13 @@ AddEventHandler('rdx:playerLoaded', function(xPlayer)
 	RDX.PlayerData = xPlayer
 end)
 
+RegisterNetEvent('rdx:onPlayerLogout')
+AddEventHandler('rdx:onPlayerLogout', function()
+	RDX.PlayerLoaded = false
+	RDX.PlayerData = {}
+	firstSpawn = true
+end)
+
 RegisterNetEvent('rdx:setJob')
 AddEventHandler('rdx:setJob', function(job)
 	RDX.PlayerData.job = job
@@ -51,11 +46,10 @@ end)
 function OnPlayerDeath()
 	IsDead = true
 	TriggerServerEvent('rdx_ambulancejob:setDeathStatus', 1)
-
-	if Config.ShowDeathTimer == true then
+    if Config.ShowDeathTimer == true then
 		ShowDeathTimer()
 	end
-
+	
 	ClearPedTasksImmediately(PlayerPedId())
 end
 
@@ -71,9 +65,11 @@ function ShowDeathTimer()
 			minutes = stringsplit(raw_minutes, ".")[1]
 			seconds = stringsplit(raw_seconds-(minutes*60), ".")[1]
 
-			DrawTxt("Você está inconsciente, mas sobreviva! Esperar"..minutes.." minutos "..seconds.." segundos ", 0.50, 0.90, 0.7, 0.7, true, 255, 255, 255, 255, true)
+			DrawTxt("Você está inconsciente, mas sobreviva! Esperar "..minutes.." minutos "..seconds.." segundos ", 0.50, 0.90, 0.7, 0.7, true, 255, 255, 255, 255, true)
 			respawnTimer = respawnTimer - 15
 		end
+		Citizen.Wait(0)
+			RemoveItemsAfterRPDeath()
 	end)
 end
 
@@ -90,12 +86,20 @@ function RemoveItemsAfterRPDeath()
 	TriggerServerEvent('rdx_ambulancejob:setDeathStatus', 0)
 
 	Citizen.CreateThread(function()
+		DoScreenFadeOut(800)
+
+		while not IsScreenFadedOut() do
+			Citizen.Wait(10)
+		end
+
 		RDX.TriggerServerCallback('rdx_ambulancejob:removeItemsAfterRPDeath', function()
+			local formattedCoords = {
+				x = Config.RespawnPoint.coords.x,
+				y = Config.RespawnPoint.coords.y,
+				z = Config.RespawnPoint.coords.z
+			}
 			RDX.SetPlayerData('loadout', {})
-
-			TriggerServerEvent('rdx:updateLastPosition')
-			RespawnPed(PlayerPedId())
-
+			RespawnPed(PlayerPedId(), formattedCoords, Config.RespawnPoint.heading)
 			DoScreenFadeIn(800)
 		end)
 	end)
